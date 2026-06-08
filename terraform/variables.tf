86b64ba572086adfc0910e9b0947170792cc75c7
Sebastian Cáceres Gelvez <sebastian.caceres.g@gmail.com>
2026-06-07 23:25:15 -0500

    infra: update terraform.tfvars and variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -4,18 +4,6 @@ variable "aws_region" {
   default     = "us-east-1"
 }
 
-# variable "bedrock_model_id" {
-#   description = "Bedrock model used for batch inference (low-cost)."
-#   type        = string
-#   default     = "amazon.nova-lite-v1:0"
-# }
-
-# variable "categories" {
-#   description = "Amazon Reviews'23 categories to ingest (MVP: a few small ones)."
-#   type        = list(string)
-#   default     = ["All_Beauty", "Gift_Cards", "Magazine_Subscriptions"]
-# }
-
 variable "project_name" {
   description = "Name prefix applied to all resource names."
   type        = string
